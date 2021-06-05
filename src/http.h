155e60f7302bb1747f419770971c6a4d3b6bea26
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2021-06-05 17:27:56 -0600

    Cleanup

--- src/http.h
+++ src/http.h
@@ -3,15 +3,15 @@
 #include <stdlib.h>
 
 // Utility string for cURL
-struct cURL_str
+struct http_str
 {
   char* data;
   size_t length;
 };
 
-int cURL_str_init(struct cURL_str* str);
-void cURL_str_free(struct cURL_str* str);
-void cURL_str_print(struct cURL_str* str);
+int http_str_init(struct http_str* str);
+void http_str_free(struct http_str* str);
+void http_str_print(struct http_str* str);
 
 // Perform an HTTP GET and grab the result to str
 // int http_get(char const* url, struct cURL_str* str);
